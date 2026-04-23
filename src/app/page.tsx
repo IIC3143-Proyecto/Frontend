e15e82a2343585ddfcf0aa83ff005b342c5cedd9
Florencia Jesús Aqueveque Saavedra <111095178+Flo-aq@users.noreply.github.com>
2026-04-23 19:36:32 -0400

    feat(auth): login page UI (#8)
    Adds the login page with full client-side validation and responsive UI.
    
    Key features:
    - Login page implementation with centralized layout.
    - Reusable FormInput component with support for icons and password toggle.
    - Form validation using Zod and React Hook Form.
    - Integration of Tabler Icons and Shadcn UI components.
    - Refined button and input styles for better UX.
    
    Co-authored-by: diego <diegomartin.lillo@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+"use client"
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <div> Hello, World! </div>
-  );
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Link href="/login" className="text-black font-black uppercase underline tracking-widest">
+        Ir al Login
+      </Link>
+    </main>
+  )
 }
