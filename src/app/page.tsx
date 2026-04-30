cdc4c1d93969e020d0f9307f0a59bbda39ac9c0c
diego <diegomartin.lillo@gmail.com>
2026-04-29 23:33:26 -0400

    chore: setup initial routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-"use client"
-import Link from "next/link"
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Link href="/login" className="text-black font-black uppercase underline tracking-widest">
-        Ir al Login
-      </Link>
-    </main>
+    <div>vtrna</div>
   )
 }
