ec49508452c3d0f3ae40ba3b53e7aa9fc01884a0
Florencia Aqueveque <fjaqueveque@uc.cl>
2026-05-10 21:11:43 -0400

    chore: remove test page and form

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OnboardingStylesForm as TestForm} from "@/components/forms/onboarding-test-form";
-
-export default function Page() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-zinc-50">
-      <TestForm />
-    </main>
-  );
-}
