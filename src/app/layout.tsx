384988ec68a87cc494c1a773a8bf1cf294e0330d
Florencia Aqueveque <fjaqueveque@uc.cl>
2026-05-20 00:26:13 -0400

    refactor: update app layout and onboarding page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="es">
       <body>
         <Providers>{children}</Providers>
+        <Toaster position="top-center" richColors />
       </body>
     </html>
   );
